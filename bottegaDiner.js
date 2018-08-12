6f25be7f10387952aa65763a2792b3964d642816
Blake Mecham <bam@Blakes-Mac-mini.local>
2018-08-11 18:48:12 -0600

    My first push of the Bottega Diner project.

---
+++ bottegaDiner.js
@@ -0,0 +1,21 @@
+const mainMenu = {
+  'steak': 12.99,
+  'country fried chicken': 10.99,
+  'ribs': 13.99,
+}
+const sideMenu = {
+  'fries': 1.99,
+  'broccoli': .99,
+  'corn': 1.59
+}
+console.log('Welcome to the Bottega Diner! Here are our main corses.')
+console.log(mainMenu)
+const mainChoice = prompt('What would you like to have tonight?')
+
+console.log('What would you like to choose as your two sides?')
+console.log(sideMenu)
+const firstSide = prompt('What will be your first choice?')
+const secondSide = prompt('What will be your second choice?')
+
+console.log(`Looks like you have chosen the ${mainChoice} with ${firstSide} and ${secondSide}.`)
+# JavaScriptProjects
